fb299e190e00ec68ce56b8da970bea691f17a21d
prototype2010 <alex.zakorko2015@gmail.com>
2020-08-13 17:14:00 +0300

    assign user to a task

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,14 +1,17 @@
 import {TypeOrmModuleOptions} from "@nestjs/typeorm";
 import {Task} from "../tasks/task.entity";
 import {User} from "../auth/user.entity";
+import * as config from 'config';
+
+const {type, port, database, synchronize, username, password, host} = config.get('db');
 
 export const typeOrmConfig : TypeOrmModuleOptions = {
-    type: "postgres",
-    host: 'localhost',
-    port: 5432,
-    username: 'test',
-    password: 'Firstday2010',
-    database: 'task_management',
+    type,
+    host,
+    port,
+    username,
+    password,
+    database,
     entities: [Task, User],
-    synchronize: true,
+    synchronize,
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const {port} = config.get('server');
+
+  await app.listen(port);
 }
 bootstrap();
