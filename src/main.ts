5046d81265a7a5733dec388a2020138acf02c599
prototype2010 <alex.zakorko2015@gmail.com>
2020-08-19 16:07:04 +0300

    cors try 25

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
 
+const whitelist = ['http://localhost:3000', 'http://68.183.75.234:3001'];
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: { origin: false, methods: ['GET','POST','PUT','DELETE','OPTIONS']}});
+  const app = await NestFactory.create(AppModule);
 
   const { port } = config.get('server');
+
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log("allowed cors for:", origin)
+        callback(null, true)
+      } else {
+        console.log("blocked cors for:", origin)
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+    credentials: true,
+  });
 
   await app.listen(port);
 }
