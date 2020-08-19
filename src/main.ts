e66330482e4a21f3154ac1d4724ce7e0d6452145
prototype2010 <alex.zakorko2015@gmail.com>
2020-08-19 18:04:51 +0300

    cors try 26

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,11 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, );
 
   const { port } = config.get('server');
 
-  app.enableCors({
-    origin: [
-      /^(.*)/,
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 200,
-    credentials: true,
-    allowedHeaders:
-        'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for',
-  });
-
+  app.enableCors();
 
   await app.listen(port);
 }
