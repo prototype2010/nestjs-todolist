dc6329727e42c6069e9033e0d7351cceee63a0d2
prototype2010 <alex.zakorko2015@gmail.com>
2020-08-25 20:27:28 +0300

    + first version build

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { ProjectsModule } from './project/projects.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -12,6 +14,9 @@ import { ProjectsModule } from './project/projects.module';
     AuthModule,
     ProjectsModule,
     ProjectsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
   ],
   providers: [],
   controllers: [],
