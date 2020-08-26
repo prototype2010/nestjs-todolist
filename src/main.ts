f4aa2673d2fe938fddf580837f3d60e30c0a04d2
prototype2010 <alex.zakorko2015@gmail.com>
2020-08-26 11:57:37 +0300

    + new build

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import {Logger} from "@nestjs/common";
 
 async function bootstrap() {
+  const logger = new Logger()
   const app = await NestFactory.create(AppModule);
 
   const { port } = config.get('server');
 
   app.enableCors();
+
+  logger.debug(`Listening to port ${port}`);
 
   await app.listen(port);
 }
